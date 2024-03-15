25d4f78a67af660875767556f75413d294c05267
ogulcan <ogyuak4@gmail.com>
2024-03-15 17:45:13 +0300

    aa

---
+++ bizimkredi/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import model.User;
+
+import javax.xml.namespace.QName;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("cem","dirman" , "password@gmail.com",
+                "321", "05542312312", "true" );
+
+    }
+}
--- bizimkredi/src/main/java/model/User.java
+++ bizimkredi/src/main/java/model/User.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.List;
+
+
 public class User {
     private String name;
     private String surname;
@@ -8,5 +12,80 @@ public class User {
     private String password; //hash fonskiyonlarından biri ile hashlanecek.
     private String phoneNumber;
     private Boolean isActive;
+
+    /*public User(String name, String surname, LocalDate birthDate, String email, String password, String phoneNumber, Boolean isActive) {
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.isActive = isActive;
+    }*/
+
+    public User(String name, String surname, String email, String password, String phoneNumber, Boolean isActive) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.isActive = isActive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
 
 }
