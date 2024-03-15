ac62292e375aa0c478b94223b1275f0292237a68
ogulcan <ogyuak4@gmail.com>
2024-03-15 23:27:41 +0300

    Kullanıcının şifresi SHA-512 ile şifrelenerek kullanıcı oluşturulmalıdır.
    Kullanıcının şifresi SHA-512 ile şifrelenerek kullanıcı oluşturuldu, stackoverflowdan faydalandım.

--- bizimkredi/src/main/java/model/User.java
+++ bizimkredi/src/main/java/model/User.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
@@ -116,5 +119,24 @@ public class User {
 
     public static void setRegisteredEmails(Set<String> registeredEmails) {
         User.registeredEmails = registeredEmails;
+    }
+
+    private String encryptPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-512");
+            byte[] encodedHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder(2 * encodedHash.length);
+            for (byte b : encodedHash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
